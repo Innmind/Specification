2db45d685b0ab5a19fa8d0a0084325f1c245b5fd
Baptiste Langlade <baptouuuu@gmail.com>
2022-02-27 15:33:51 +0100

    transform Operator and Sign into enums

--- src/Operator.php
+++ src/Operator.php
@@ -6,46 +6,8 @@ namespace Innmind\Specification;
 /**
  * @psalm-immutable
  */
-final class Operator
+enum Operator
 {
-    private const AND = 'AND';
-    private const OR = 'OR';
-
-    private static ?self $and = null;
-    private static ?self $or = null;
-
-    private $operator;
-
-    private function __construct(string $operator)
-    {
-        $this->operator = $operator;
-    }
-
-    public function __toString(): string
-    {
-        return $this->operator;
-    }
-
-    /**
-     * @psalm-pure
-     */
-    public static function and(): self
-    {
-        /** @psalm-suppress ImpureStaticProperty */
-        return self::$and ??= new self(self::AND);
-    }
-
-    /**
-     * @psalm-pure
-     */
-    public static function or(): self
-    {
-        /** @psalm-suppress ImpureStaticProperty */
-        return self::$or ??= new self(self::OR);
-    }
-
-    public function equals(self $self): bool
-    {
-        return (string) $this === (string) $self;
-    }
+    case and;
+    case or;
 }
